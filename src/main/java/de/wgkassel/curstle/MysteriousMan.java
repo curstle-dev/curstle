1191782119513b13968e766b4419b3a7375c29d7
jgehle <judige@freenet.de>
2019-07-01 18:56:51 +0200

    mysterious man

--- src/main/java/de/wgkassel/curstle/MysteriousMan.java
+++ src/main/java/de/wgkassel/curstle/MysteriousMan.java
@@ -1,9 +1,17 @@
 package de.wgkassel.curstle;
 
+import de.wgkassel.curstle.Worlds.Screens.StoryRoom;
 import greenfoot.Actor;
+import greenfoot.Greenfoot;
 
 public class MysteriousMan extends Actor {
     public MysteriousMan() {
-        
     }
+        @Override
+        public void act() {
+            if (Greenfoot.mouseClicked(this)){
+                StoryRoom storyRoom = new StoryRoom(1700,100,1);
+                Greenfoot.setWorld(storyRoom);
+            }
+        }
 }
---
+++ src/main/java/de/wgkassel/curstle/Worlds/Screens/StoryRoom.java
@@ -0,0 +1,9 @@
+package de.wgkassel.curstle.Worlds.Screens;
+
+import greenfoot.World;
+
+public class StoryRoom extends World {
+    public StoryRoom(int worldWidth, int worldHeight, int cellSize) {
+        super(worldWidth, worldHeight, cellSize);
+    }
+}
