d574ae83b68b790da10926986ce4f2ddd9af9114
jgehle <judige@freenet.de>
2019-07-03 17:41:04 +0200

    trying to remove text

--- src/main/java/de/wgkassel/curstle/Worlds/Screens/StoryRoom.java
+++ src/main/java/de/wgkassel/curstle/Worlds/Screens/StoryRoom.java
@@ -3,6 +3,7 @@ package de.wgkassel.curstle.Worlds.Screens;
 import de.wgkassel.curstle.MysteriousMan;
 import de.wgkassel.curstle.Speechbubble;
 import de.wgkassel.curstle.Story;
+import de.wgkassel.curstle.Torch;
 import de.wgkassel.curstle.Worlds.Level1.BaseWorld;
 import de.wgkassel.curstle.player.Player;
 import greenfoot.Greenfoot;
@@ -11,6 +12,15 @@ public class StoryRoom extends BaseWorld {
 
     int story = 1;
     private ChangeCharacter cc;
+
+    boolean existStory1 = false;
+    boolean existStory2 = false;
+    boolean existStory3 = false;
+    boolean existStory4 = false;
+    boolean existStory5 = false;
+    boolean existStory6 = false;
+    boolean existStory7 = false;
+    boolean existStory8 = false;
 
     public StoryRoom(Player player) {
         super(player);
@@ -30,6 +40,9 @@ public class StoryRoom extends BaseWorld {
         cc = new ChangeCharacter();
         addObject(cc, 1000, 500);
 
+        Torch torch = new Torch();
+        addObject(torch, 400, 400);
+
     }
 
 
@@ -37,39 +50,70 @@ public class StoryRoom extends BaseWorld {
         if (Greenfoot.mouseClicked(cc)) {
             story++;
         }
+        Story story1 = new Story(1);
+        Story story2 = new Story(2);
+        Story story3 = new Story(3);
+        Story story4 = new Story(4);
+        Story story5 = new Story(5);
+        Story story6 = new Story(6);
+        Story story7 = new Story(7);
+        Story story8 = new Story(8);
 
+        if (existStory2) {
+            removeObject(story1);
+        }
+
+        if (existStory3) {
+            removeObject(story2);
+        }
+
+        if (existStory4) {
+            removeObject(story3);
+        }
+        if (existStory5) {
+            removeObject(story4);
+        }
+        if (existStory6) {
+            removeObject(story5);
+        }
+        if (existStory7) {
+            removeObject(story6);
+        }
+        if (existStory8) {
+            removeObject(story7);
+        }
         if (story == 1) {
-            Story story = new Story(1);
-            addObject(story, 800, 500);
+            addObject(story1, 800, 500);
+            existStory1 = true;
         }
 
         if (story == 2) {
-            Story story = new Story(2);
-            addObject(story, 800, 500);
+            existStory2 = true;
+            addObject(story2, 800, 500);
         }
         if (story == 3) {
-            Story story = new Story(3);
-            addObject(story, 800, 500);
+            existStory3 = true;
+            addObject(story3, 800, 500);
         }
         if (story == 4) {
-            Story story = new Story(4);
-            addObject(story, 800, 500);
+            existStory4 = true;
+            addObject(story4, 800, 500);
         }
         if (story == 5) {
-            Story story = new Story(5);
-            addObject(story, 800, 500);
+            existStory5 = true;
+            addObject(story5, 800, 500);
         }
         if (story == 6) {
-            Story story = new Story(6);
-            addObject(story, 800, 500);
+            existStory6 = true;
+            addObject(story6, 800, 500);
         }
         if (story == 7) {
-            Story story = new Story(7);
-            addObject(story, 800, 500);
+            existStory7 = true;
+            addObject(story7, 800, 500);
         }
         if (story == 8) {
-            Story story = new Story(8);
-            addObject(story, 800, 500);
+            existStory8 = true;
+            addObject(story8, 800, 500);
         }
     }
 }
