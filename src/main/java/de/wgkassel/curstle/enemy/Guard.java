b2dba423ca3479e28ca6d80897e686400a4f0d63
MaxRavensFlight <mbeckenbach@gmx.de>
2019-07-04 17:36:47 +0200

    Boss2Enemy hat nun bild

---
+++ src/main/java/de/wgkassel/curstle/enemy/Guard.java
@@ -0,0 +1,15 @@
+package de.wgkassel.curstle.enemy;
+
+import greenfoot.Actor;
+
+public class Guard extends Actor {
+    public Guard(){
+        setImage("Guard.png");
+    }
+    public void act(){
+
+    }
+    public void moveUrAss(){
+
+    }
+}
