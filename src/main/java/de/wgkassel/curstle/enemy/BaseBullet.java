ecbb8a666c66ea80252a37b1f6fad197dee45d7f
StinkeNudel <henrikstarke@yahoo.de>
2019-07-27 11:18:47 +0200

    finished BaseBullet

--- src/main/java/de/wgkassel/curstle/enemy/BaseBullet.java
+++ src/main/java/de/wgkassel/curstle/enemy/BaseBullet.java
@@ -5,33 +5,43 @@ import greenfoot.Actor;
 
 public class BaseBullet extends Actor {
 
-    private int speed;
-    private boolean hitPlayer;
+    public int speed;
+    private boolean hitPlayer = false;
     long wait = System.currentTimeMillis();
+    public boolean removeMe = false;
 
     @Override
     public void act() {
         super.act();
         move(speed);
+        checkRemove();
         checkPlayer();
         checkRemove();
+        removeMe();
     }
 
-    public void checkPlayer(){
-       if(!getIntersectingObjects(Player.class).isEmpty()){
-           hitPlayer = true;
-       }
-    }
-
-    public void checkRemove(){
-        if (isAtEdge() ) {
-            getWorld().removeObject(this);
-        }
-        if (hitPlayer){
+    public void checkPlayer() {
+        if (!hitPlayer && !getIntersectingObjects(Player.class).isEmpty()) {
+            hitPlayer = true;
             wait = System.currentTimeMillis();
-            if (System.currentTimeMillis() - wait > 500) {
-                getWorld().removeObject(this);
+        }
+    }
+
+    public void checkRemove() {
+        if (hitPlayer) {
+            if (System.currentTimeMillis() - wait > 10) {
+                removeMe = true;
             }
+        } else if (isAtEdge()) {
+            getWorld().removeObject(this);
+            removeMe = true;
+        }
+
+    }
+
+    public void removeMe(){
+        if (removeMe){
+            getWorld().removeObject(this);
         }
     }
 }
--- src/main/java/de/wgkassel/curstle/enemy/ShotgunShot.java
+++ src/main/java/de/wgkassel/curstle/enemy/ShotgunShot.java
@@ -2,10 +2,8 @@ package de.wgkassel.curstle.enemy;
 
 import greenfoot.Actor;
 
-public class ShotgunShot extends Actor {
+public class ShotgunShot extends BaseBullet {
     private long counter;
-
-    int speed;
 
     public ShotgunShot(int x) {
         counter = System.currentTimeMillis();
@@ -15,10 +13,7 @@ public class ShotgunShot extends Actor {
     @Override
     public void act() {
         super.act();
-        move(speed);
-        if (isAtEdge()) {
-            getWorld().removeObject(this);
-        } else if (System.currentTimeMillis() - counter > 700) {
+        if (System.currentTimeMillis() - counter > 700) {
             getWorld().removeObject(this);
         }
     }
