920ecc5198851d3694d9975173c6eb6bf40fca96
StinkeNudel <henrikstarke@yahoo.de>
2019-07-30 16:08:51 +0200

    Mage redesign

--- src/main/java/de/wgkassel/curstle/player/Mage.java
+++ src/main/java/de/wgkassel/curstle/player/Mage.java
@@ -5,7 +5,7 @@ import greenfoot.Greenfoot;
 public class Mage extends Player {
 
     int cooldown = 40;
-
+    int walkAnimation = 0;
 
     public Mage() {
         setImage("Mage.png");
@@ -24,16 +24,16 @@ public class Mage extends Player {
         if (Greenfoot.isKeyDown("up")) {
             MageShot mshot1 = new MageShot();
             getWorld().addObject(mshot1, this.getX(), this.getY() - 50);
-            setImage("Mage_back.png");
-            this.getImage().scale(120, 130);
+            setImage("MageBack.png");
+            this.getImage().scale(128, 216);
             mshot1.setRotation(-90);
             cooldown = 0;
         }
         if (Greenfoot.isKeyDown("right")) {
             MageShot mshot2 = new MageShot();
             getWorld().addObject(mshot2, this.getX() + 50, this.getY());
-            setImage("Mage_right.png");
-            this.getImage().scale(120, 130);
+            setImage("MageRight.png");
+            this.getImage().scale(128, 216);
             mshot2.setRotation(0);
             cooldown = 0;
         }
@@ -41,15 +41,15 @@ public class Mage extends Player {
             MageShot mshot3 = new MageShot();
             getWorld().addObject(mshot3, this.getX(), this.getY() + 50);
             setImage("Mage.png");
-            this.getImage().scale(120, 130);
+            this.getImage().scale(128, 216);
             mshot3.setRotation(90);
             cooldown = 0;
         }
         if (Greenfoot.isKeyDown("left")) {
             MageShot mshot4 = new MageShot();
             getWorld().addObject(mshot4, this.getX() - 50, this.getY());
-            setImage("Mage_left.png");
-            this.getImage().scale(120, 130);
+            setImage("MageLeft.png");
+            this.getImage().scale(128, 216);
             mshot4.setRotation(-180);
             cooldown = 0;
         }
@@ -58,17 +58,52 @@ public class Mage extends Player {
     public void looking() {
 
         if (Greenfoot.isKeyDown("w")) {
-            setImage("Mage_back.png");
-            this.getImage().scale(120, 130);
+            if (walkAnimation <= 5) {
+                setImage("MageBackWalk1.png");
+                walkAnimation = walkAnimation + 1;
+            } else if (walkAnimation > 5 && walkAnimation <= 10) {
+                setImage("MageBackWalk2.png");
+                walkAnimation = walkAnimation + 1;
+            } else {
+                walkAnimation = 0;
+            }
+            this.getImage().scale(128, 216);
         } else if (Greenfoot.isKeyDown("d")) {
-            setImage("Mage_right.png");
-            this.getImage().scale(120, 130);
+            if (walkAnimation <= 5) {
+                setImage("MageRightWalk1.png");
+                walkAnimation = walkAnimation + 1;
+            } else if (walkAnimation > 5 && walkAnimation <= 10) {
+                setImage("MageRightWalk2.png");
+                walkAnimation = walkAnimation + 1;
+            } else {
+                walkAnimation = 0;
+            }
+            this.getImage().scale(128, 216);
         } else if (Greenfoot.isKeyDown("a")) {
-            setImage("Mage_left.png");
-            this.getImage().scale(120, 130);
+            if (walkAnimation <= 5) {
+                setImage("MageLeftWalk1.png");
+                walkAnimation = walkAnimation + 1;
+            } else if (walkAnimation > 5 && walkAnimation <= 10) {
+                setImage("MageLeftWalk2.png");
+                walkAnimation = walkAnimation + 1;
+            } else {
+                walkAnimation = 0;
+            }
+            this.getImage().scale(128, 216);
         } else if (Greenfoot.isKeyDown("s")) {
+            if (walkAnimation <= 5) {
+                setImage("MageFrontWalk1.png");
+                walkAnimation = walkAnimation + 1;
+            } else if (walkAnimation > 5 && walkAnimation <= 10) {
+                setImage("MageFrontWalk2.png");
+                walkAnimation = walkAnimation + 1;
+            } else {
+                walkAnimation = 0;
+            }
+            this.getImage().scale(128, 216);
+        } else if (!upPressed && !downPressed && !leftPressed && !rightPressed && !Greenfoot.isKeyDown("right") && !Greenfoot.isKeyDown("left") && !Greenfoot.isKeyDown("up") && !Greenfoot.isKeyDown("down")) {
             setImage("Mage.png");
-            this.getImage().scale(120, 130);
+            this.getImage().scale(128, 216);
         }
     }
 }
